025035918fc8d868c5f9404ea35033dd9cd6a975
dmalay <dmalay1228@gmail.com>
2021-06-06 14:59:31 +0300

    added routing, tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Main from './components/main'
+import SignIn from './components/signin'
+import SignUp from './components/signup'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Switch>
+        <Route exact path="/" component={() => <Main />} />
+        <Route exact path="/signin" component={() => <SignIn />} />
+        <Route exact path="/signup" component={() => <SignUp />} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
